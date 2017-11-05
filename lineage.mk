77bce1e6d1ccafbed9b1bb3a260f63ad419e566e
Cswl Coldwind <cswl1337@gmail.com>
2017-11-05 17:50:05 +0545

    [nougat-only] Revert to vendor/cm path for lineage-14.1
    Change-Id: I6dade19504967b977ff3b9f559ae2aa84d886ec4

--- lineage.mk
+++ lineage.mk
@@ -21,7 +21,7 @@ TARGET_LOW_RAM_DEVICE := true
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
 # Inherit some common CM stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+$(call inherit-product, vendor/cm/config/common_full_phone.mk)
 
 # Inherit device configuration
 $(call inherit-product, device/samsung/galaxysmtd/device.mk)
